aaa1d49d46506795be9191faaa1d9784ef5805d2
NicoDS09 <nicolas.dossants@laposte.net>
2021-02-06 00:32:54 +0100

    tests for user

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,4 +7,24 @@ router.get("/", (req, res, next) => {
 	res.status(200).send("ok\n")
 })
 
+router.get("/user", (req, res, next) => {
+	res.status(200).json([{"success":true,
+	"code":200,
+	"message":"Ok",
+	"data": [{id: "toto",email:"toto@toto.fr",role:"chef",tel:"09090909"}]	
+	}])
+})
+
+router.post("/user", (req, res, next) => {
+	res.status(201).json(req.body)
+})
+
+router.put("/user", (req, res, next) => {
+	res.status(201).json(req.body)
+})
+
+router.delete("/user", (req, res, next) => {
+	res.status(200).json(req.body)
+})
+
 module.exports = router
