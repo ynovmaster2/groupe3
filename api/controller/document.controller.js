23c093720f0773f99d5b75d11155db10f13c64ff
NicoDS09 <nicolas.dossants@laposte.net>
2021-02-07 15:35:31 +0000

    crud for document model

--- api/controller/document.controller.js
+++ api/controller/document.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const Document = require("../models/document").model
 
 exports.create = (req, res) => {
@@ -13,4 +14,89 @@ exports.create = (req, res) => {
 		},
 		(e) => res.status(500).send(e)
 	)
+}
+
+exports.findAll = (req, res) => {
+	Document.find()
+		.then((document) => {
+			res.status(200).send(document)
+		})
+		.catch((err) => {
+			res.status(500).send({
+				message: err.message,
+			})
+		})
+}
+
+exports.findOne = (req, res) => {
+	Document.findById(req.params.documentId)
+		.then((document) => {
+			if (!document) {
+				return res.status(404).send({
+					message: `Document not found with id ${req.params.documentId}`,
+				})
+			}
+			res.send(document)
+		})
+		.catch((err) => {
+			if (err.kind === "ObjectId") {
+				return res.status(404).send({
+					message: `Docuement not found with id ${req.params.documentId}`,
+				})
+			}
+			return res.status(500).send({
+				message: `Error retrieving document with id ${req.params.documentId}`,
+			})
+		})
+}
+
+exports.update = (req, res) => {
+	Document.findByIdAndUpdate(
+		req.params.documentId,
+		{
+			nom: req.body.nom,
+			path: req.body.path,
+		},
+		{ new: true }
+	)
+		.then((document) => {
+			if (!document) {
+				return res.status(404).send({
+					message: `Note not found with id ${req.params.documentId}`,
+				})
+			}
+			res.send(document)
+		})
+		.catch((err) => {
+			if (err.kind === "ObjectId") {
+				return res.status(404).send({
+					message: `Note not found with id ${req.params.documentId}`,
+				})
+			}
+			return res.status(500).send({
+				message: `Error updating note with id ${req.params.documentId}`,
+			})
+		})
+}
+
+exports.deleteDocument = (req, res) => {
+	Document.findByIdAndRemove(req.params.documentId)
+		.then((document) => {
+			if (!document) {
+				return res.status(404).send({
+					message: `Document not found with id ${req.params.documentId}`,
+				})
+			}
+			res.send({ message: "Document deleted successfully!" })
+		})
+		.catch((err) => {
+			if (err.kind === "ObjectId" || err.name === "NotFound") {
+				return res.status(404).send({
+					message: `Document not found with id ${req.params.documentId}`,
+				})
+			}
+			return res.status(500).send({
+				message: `Could not delete document with id ${req.params.documentId}`,
+			})
+		})
 }
--- api/routes/document.js
+++ api/routes/document.js
@@ -1,34 +1,20 @@
 const express = require("express")
 const { create } = require("../controller/document.controller")
+const { findAll } = require("../controller/document.controller")
+const { findOne } = require("../controller/document.controller")
+const { update } = require("../controller/document.controller")
+const { deleteDocument } = require("../controller/document.controller")
 
 const router = express.Router()
 
-router.get("", (req, res) => {
-	res.status(200).json([
-		{
-			success: true,
-			code: 200,
-			message: "Ok",
-			data: [
-				{
-					nom: "projet",
-					path: "https://projet.com",
-					createdAt: "1612606573",
-					updatedAt: "1612606573",
-				},
-			],
-		},
-	])
-})
+router.get("", findAll)
+
+router.get("/:documentId", findOne)
 
 router.post("", create)
 
-router.put("", (req, res) => {
-	res.status(201).json(req.body)
-})
+router.put("/:documentId", update)
 
-router.delete("", (req, res) => {
-	res.status(200).json(req.body)
-})
+router.delete("/:documentId", deleteDocument)
 
 module.exports = router
