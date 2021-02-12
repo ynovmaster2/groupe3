bb4c363a627a7438c0664115b7ffc64f8868dfaa
NicoDS09 <nicolas.dossants@laposte.net>
2021-02-12 21:54:08 +0100

    fix bootsrapp

--- web/pages/index.js
+++ web/pages/index.js
@@ -6,16 +6,6 @@ import Header from "./header"
 export default function Home() {
 	return (
 		<React.Fragment>
-			<Head>
-				<title>OrderProjects</title>
-				<link
-					rel="stylesheet"
-					href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-				></link>
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-			</Head>
 			<Header></Header>
 		</React.Fragment>
 	)
