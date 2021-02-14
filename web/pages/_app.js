a3dc1f68a5f3d8f902a75620b50ce6939dcaef96
NicoDS09 <nicolas.dossants@laposte.net>
2021-02-14 23:19:32 +0000

    navbar

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,13 @@
-import "../styles/globals.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from "../components/header"
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<div>
+			<Header />
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
